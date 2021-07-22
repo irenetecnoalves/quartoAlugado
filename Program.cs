0ad31cd643f0df8a80cd9a3fa6521b5341485961
irenetecnoalves <irenetecnosenior@gmail.com>
2021-07-22 20:21:06 -0300

    exercício para aprendizagem da linguagem c#

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using pensionato.obj.Classes;
+
+namespace pensionato
+{
+    internal class NewBaseType
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a quantidade de quartos que será alugado");
+            int n = int.Parse(Console.ReadLine());
+
+            Quarto[] vect = new Quarto[10];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Digite o nome do estudante");
+                string nome = Console.ReadLine();
+
+                Console.WriteLine("Digite o Email do estudante");
+                string email = Console.ReadLine();
+
+                Console.WriteLine("Digite o Quarto escolhido pelo estudante");
+                int quarto =int.Parse(Console.ReadLine());
+                vect[quarto]= new Quarto(nome,email);
+                 Console.WriteLine();
+        }
+         Console.WriteLine("Listagem dos Quartos Alugados:");
+        for(int q = 0; q <10 ; q++){
+
+            if (vect[q] != null ){
+
+            Console.WriteLine( q +" :" +vect[q]);
+            }
+        }
+    }
+
+}
+   
+}
---
+++ Quarto.cs
@@ -0,0 +1,21 @@
+namespace pensionato.obj.Classes
+{
+    public class Quarto
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+    
+
+        public Quarto(string nome, string email)
+        {
+            Nome = nome;
+            Email = email;
+
+        }
+        public override string ToString() {
+         return Nome + ", " + Email;
+    }  
+
+}
+}
